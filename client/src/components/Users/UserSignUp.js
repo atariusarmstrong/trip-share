26a5dfaf7b1bcd7a2583b622748d2cdc730f46ba
Atarius Armstrong <atariusarmstrong@gmail.com>
2019-01-28 11:50:28 -0500

    UserSignUp add new user to the database -- now to redirect on submission

--- client/src/components/Users/UserSignUp.js
+++ client/src/components/Users/UserSignUp.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components'
+import axios from 'axios'
 
 const Container = styled.div`
     width: 800px;
@@ -23,10 +24,18 @@ class UserSignUp extends Component {
 
     handleChange = (e) => {
         const newState = {...this.state.user}
-        console.log(newState)
+        // console.log(newState)
         newState[e.target.name] = e.target.value
         this.setState({user: newState})
-        
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        const payload = this.state.user
+        axios.post('/api/signup', payload)
+        console.log((res) => {
+            console.log(res.data)
+        })
     }
 
 
@@ -35,10 +44,10 @@ class UserSignUp extends Component {
             <div>
                 <Container>
                     <h2>Tell us about yourself</h2>
-                    <form>
-                        <input type="text" name="username" placeholder="username" onChange={this.handleChange}/><br/>
-                        <input type="text" name="location" placeholder="Where are you from?" onChange={this.handleChange}/><br/>
-                        <input type="text" name="dreamTrip" placeholder="Where's your dream destination?" onChange={this.handleChange}/><br/>
+                    <form onSubmit={this.handleSubmit}>
+                        <input type="text" name="username" value={this.state.user.username} placeholder="username" onChange={this.handleChange}/><br/>
+                        <input type="text" name="location" value={this.state.user.location} placeholder="Where are you from?" onChange={this.handleChange}/><br/>
+                        <input type="text" name="dreamTrip" value={this.state.user.dreamTrip} placeholder="Where's your dream destination?" onChange={this.handleChange}/><br/>
                         <input type="checkbox"/> Passport?
                         <br/>
                         <button type="submit">Sign Up</button>
