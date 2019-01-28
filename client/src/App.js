29f25ff1945182be8f2b8db74527b600fc884676
Atarius Armstrong <atariusarmstrong@gmail.com>
2019-01-28 10:29:42 -0500

    landing page rendered

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import LandingPage from './components/LandingPage';
+
 
 
 
@@ -6,7 +9,11 @@ class App extends Component {
   render() {
     return (
       <div>
-        Hello World
+       <Router>
+         <Switch>
+           <Route exact path="/" component={LandingPage} />
+         </Switch>
+       </Router>
       </div>
     );
   }
---
+++ client/src/components/LandingPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class LandingPage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>TripShare</h1>
+                <p>Let's plan your next trip together.</p>
+            </div>
+        );
+    }
+}
+
+export default LandingPage;
