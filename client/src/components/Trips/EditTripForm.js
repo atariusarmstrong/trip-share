b781ef14b1156224b025436198c99f6d388981ae
Atarius Armstrong <atariusarmstrong@gmail.com>
2019-01-29 20:24:51 -0500

    trip edit form updates assigned trip

--- client/src/components/Trips/EditTripForm.js
+++ client/src/components/Trips/EditTripForm.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+import NavBar from '../NavBar'
 
 class EditTripForm extends Component {
+    state = {
+        trip: {
+            destination: "",
+            from: "",
+            to: "",
+            accomodation: "",
+            transportation: "",
+        }
+    }
+
+    handleChange = (e) => {
+        const newState = {...this.state.trip}
+        newState[e.target.name] = e.target.value
+        this.setState({trip: newState})
+        // console.log(newState)
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        const payload = this.state.trip
+        const tripId = this.props.match.params.tripId
+        axios.patch(`/api/trips/${tripId}`, payload)
+        console.log((res) => {
+            console.log(res.data)
+        })
+    }
     render() {
         return (
             <div>
-                We're gonna edit a trip
+                <NavBar />
+                <div>
+                    <h2>Where are you going?</h2>
+                    <form onSubmit={this.handleSubmit}>
+                        <input type="text" name="destination" value={this.state.trip.destination}n placeholder="Destination" onChange={this.handleChange}/><br/>
+                        <input type="date" name="from" value={this.state.trip.from} onChange={this.handleChange}/><br/>
+                        <input type="date" name="to" value={this.state.trip.to} onChange={this.handleChange}/><br/>
+                        <input type="text" name="accomodation" value={this.state.trip.accomodation} placeholder="Accomodation" onChange={this.handleChange}/><br/>
+                        <input type="text" name="transportation" value={this.state.trip.transportation} placeholder="Transportation" onChange={this.handleChange}/><br/>
+                        <button>Let's Go!</button>
+                    </form>
+                </div>
             </div>
         );
     }
