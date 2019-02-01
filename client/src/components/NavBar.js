da4f07bcce404f3697a8eb324f1a8362e1adc12e
Atarius Armstrong <atariusarmstrong@gmail.com>
2019-01-31 21:24:15 -0500

    fixed nav bar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,16 +2,35 @@ import React, { Component } from 'react';
 import styled from 'styled-components'
 import {Link} from 'react-router-dom'
 
-const Navigation = styled.ul`
+const Navigation = styled.div`
     background: #D5FFFF;
-    list-style-type: none;
     display: flex;
+    justify-content: space-between;
+`
 
+const LinkList = styled.ul`
+    font-family: Avenir;
+    display: flex;
+    list-style-type: none;
+    text-transform: uppercase;
+    
+    a {
+        text-decoration: none;
+        color: #707070;
+        &:hover{
+            color: #00e6e6;
+        }
+    }
+    li{
+        margin: 10px;
+        float: right;
+    }
 `
 const Header = styled.h1`
     font-family: SignPainter;
     font-size: 40px
     color: #707070;
+    margin-left: 10px;
 `
 
 class NavBar extends Component {
@@ -21,9 +40,12 @@ class NavBar extends Component {
                 
                 <Navigation>
                     <Header>TripShare</Header>
-                    <Link to="/trips"><li>Trips</li></Link>
-                    <Link to='/groups'><li>Groups</li></Link>
-                    <Link to='/users'><li>Travelers</li></Link>
+                    <LinkList>
+                        <Link to="/trips"><li>Trips</li></Link>
+                        <Link to='/groups'><li>Groups</li></Link>
+                        <Link to='/users'><li>Travelers</li></Link>
+                    </LinkList>
+                    
                 </Navigation>
             </div>
         );
